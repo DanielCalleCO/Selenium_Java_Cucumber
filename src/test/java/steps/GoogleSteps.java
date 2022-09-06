f3e64deea78037ca1bf40ed5f6824ca3f830b2bc
Daniel Calle <danielcalleco@gmail.com>
2022-09-06 15:06:46 -0500

    First commit

--- src/test/java/steps/GoogleSteps.java
+++ src/test/java/steps/GoogleSteps.java
@@ -1,13 +1,19 @@
 package steps;
 
 import io.cucumber.java.en.*;
+import pages.GooglePage;
 
 public class GoogleSteps {
+
+    GooglePage google = new GooglePage();
+
     @Given("^I am on the Google search page$")
     public void navigateToGoogle(){
+        google.navigatoToGoogle();
+        google.clickGoogleSearch();
     }
     
-    @When("^I enter a search button$")
+    @When("^I enter a search criteria$")
     public void enterSearchCriteria(){
     }
 
